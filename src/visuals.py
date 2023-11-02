034132fb5f700899ab21a47b35c6ed7320df47af
johmakinen <joh.makinen@gmail.com>
2023-11-02 15:55:28 +0200

    chore: test adding pre-commit hooks

--- src/visuals.py
+++ src/visuals.py
@@ -1,11 +1,11 @@
 """Visuals"""
-import sys
-from typing import Iterable, Union
-import numpy as np
-import logging
-from src.evolutionary import Agent
+from typing import Iterable
+from typing import Union
 
+import numpy as np
 import plotly.graph_objects as go
+
+from src.evolutionary import Agent
 
 # logger = init_logger(level_=logging.WARNING, log_file="genetic_logs.log")
 # seed_all(421)
@@ -36,13 +36,17 @@ def visualise_evolution(
     y_lines = y_lines.T
 
     # create the scatter plot
-    points = go.Scatter(x=x, y=y, mode="markers")
+    points = go.Scatter(x=x, y=y, mode="markers", name="Data")
 
     # create initial line
-    line = go.Scatter(x=x, y=y_lines[0])
+    line = go.Scatter(x=x, y=y_lines[0], name=f"Iteration {0}")
 
     # create a layout with out title (optional)
-    layout = go.Layout(title_text="Evolutionary algorithm animation")
+    layout = go.Layout(
+        title_text="Evolutionary algorithm animation",
+        yaxis=dict(range=[min(y) * 1.1, max(y) * 1.1]),
+        xaxis=dict(range=[min(x) * 1.1, max(x) * 1.1]),
+    )
 
     # combine the graph_objects into a figure
     fig = go.Figure(data=[points, line])
@@ -52,13 +56,21 @@ def visualise_evolution(
 
     # create a frame for every line y
     for i, y_line in enumerate(y_lines):
-        neg_error = np.clip(np.array(y-y_line),0,np.inf)
-        pos_error = np.abs(np.clip(np.array(y-y_line),-np.inf,0))
+        neg_error = np.clip(np.array(y - y_line), 0, np.inf)
+        pos_error = np.abs(np.clip(np.array(y - y_line), -np.inf, 0))
         # update the line
         line = go.Scatter(
             x=x,
             y=y_line,
-            error_y={"type": "data", "symmetric": False, "array": neg_error, "arrayminus": pos_error,"color":"#ff0000"},line=dict(color="#b900ff")
+            error_y={
+                "type": "data",
+                "symmetric": False,
+                "array": neg_error,
+                "arrayminus": pos_error,
+                "color": "#ff0000",
+            },
+            line=dict(color="#b900ff"),
+            name=f"Iteration {i}",
         )
 
         # create the button
@@ -68,18 +80,21 @@ def visualise_evolution(
                 {
                     "label": "Play",
                     "method": "animate",
-                    "args": [None, {"frame": {"duration": 35}}],
+                    "args": [None, {"frame": {"duration": 70}}],
                 }
             ],
         }
 
         # add the button to the layout and update the
         # title to show the gradient descent step
-        layout = go.Layout(updatemenus=[button], title_text=f"Evolution Step {i}")
+        layout = go.Layout(
+            updatemenus=[button], title_text=f"Evolution Step {i}"
+        )
 
         # create a frame object
         frame = go.Frame(
-            data=[points, line], layout=go.Layout(title_text=f"Evolution Step {i}")
+            data=[points, line],
+            layout=go.Layout(title_text=f"Evolution Step {i}"),
         )
 
         # add the frame object to the frames list
